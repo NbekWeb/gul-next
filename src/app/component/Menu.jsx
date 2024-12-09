349009a117886f590f5a0c9fa1f0e192e75344a7
NbekWeb <nbekdev@gmail.com>
2024-12-09 13:03:52 +0500

    sa

--- src/app/component/Menu.jsx
+++ src/app/component/Menu.jsx
@@ -101,8 +101,8 @@ export default function Menus() {
 
   return (
     <div className={`relative `}>
-      <div className="container max-sm:px-3  px-5 mx-auto overflow-x-hidden ">
-        <div className="flex items-center justify-between py-5">
+      <div className="container px-5 mx-auto overflow-x-hidden max-sm:px-3 ">
+        <div className="flex items-center justify-between py-5 max-md:grid max-md:grid-cols-3">
           <div className="flex items-center gap-4 md:hidden">
             <span className="text-4xl" onClick={() => setOpen(true)}>
               <Icon type="menu" />
@@ -113,41 +113,17 @@ export default function Menus() {
                 <Icon type="tel" />
               </span>
             </Popover>
-            <div className="flex items-center none-select md:hidden">
-              <Select
-                value={selectedLang}
-                className=""
-                onChange={(val) => changeLang(val)}
-                suffixIcon={
-                  <span className="bg-pink-500 text-[8px] text-white w-5 h-5 flex justify-center items-center rounded-full">
-                    <svg
-                      width="1em"
-                      height="1em"
-                      viewBox="0 0 10 6"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M8.85671 0.70194C8.68607 0.701976 8.52244 0.76979 8.4018 0.890465L5.45102 3.84124C5.32868 3.95866 5.16568 4.02423 4.99612 4.02423C4.82655 4.02423 4.66355 3.95866 4.54121 3.84124L1.59043 0.890465C1.46908 0.773259 1.30655 0.708405 1.13784 0.70987C0.969135 0.711337 0.807754 0.779006 0.688457 0.898303C0.56916 1.0176 0.50149 1.17898 0.500024 1.34769C0.498558 1.51639 0.563413 1.67892 0.680619 1.80028L3.6314 4.75106C3.99919 5.10203 4.48805 5.29785 4.99644 5.29785C5.50483 5.29785 5.99368 5.10203 6.36148 4.75106L9.31161 1.80028C9.40157 1.71029 9.46283 1.59566 9.48764 1.47086C9.51246 1.34607 9.49972 1.21671 9.45103 1.09916C9.40234 0.981605 9.31989 0.881124 9.21411 0.810421C9.10832 0.739718 8.98394 0.701967 8.85671 0.70194Z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                  </span>
-                }
-                style={{ width: 70 }}
-              >
-                <Select.Option value="ru">RU</Select.Option>
-                <Select.Option value="en">EN</Select.Option>
-              </Select>
-            </div>
           </div>
-          <Link href={`/`} className="flex items-center gap-2.5">
+          <Link
+            href={`/`}
+            className="flex items-center gap-2.5 max-md:justify-center"
+          >
             <img src="/img/logo.png" alt="logo" className="w-10" />
             <span className="text-xl font-medium text-pink-500 max-sm:hidden max-md:text-base">
               FLOWERS&OPT
             </span>
           </Link>
-          <div className="flex gap-4 text-xl text-dark-400 md:hidden">
+          <div className="flex gap-4 text-xl text-dark-400 md:hidden max-md:justify-end">
             <Link href={`/favorite`}>
               <Icon type="heart" />
             </Link>
@@ -161,7 +137,7 @@ export default function Menus() {
             </Link>
             <Icon type="user" />
           </div>
-          <div className="flex items-center gap-4 max-md:hidden">
+          <div className="flex items-center gap-4 max-md:hidden ">
             <div className="flex items-center none-select">
               <Select
                 value={selectedLang}
@@ -312,6 +288,63 @@ export default function Menus() {
             <Icon type="close" />
           </div>
           <div className="pr-6">
+            <div className="flex items-center gap-4 pl-6 mb-2">
+              <div className="flex items-center none-select">
+                <Select
+                  value={selectedLang}
+                  className=""
+                  onChange={(val) => {
+                    changeLang(val);
+                  }}
+                  suffixIcon={
+                    <span className="bg-pink-500 text-[8px] text-white w-5 h-5 flex justify-center items-center rounded-full">
+                      <svg
+                        width="1em"
+                        height="1em"
+                        viewBox="0 0 10 6"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M8.85671 0.70194C8.68607 0.701976 8.52244 0.76979 8.4018 0.890465L5.45102 3.84124C5.32868 3.95866 5.16568 4.02423 4.99612 4.02423C4.82655 4.02423 4.66355 3.95866 4.54121 3.84124L1.59043 0.890465C1.46908 0.773259 1.30655 0.708405 1.13784 0.70987C0.969135 0.711337 0.807754 0.779006 0.688457 0.898303C0.56916 1.0176 0.50149 1.17898 0.500024 1.34769C0.498558 1.51639 0.563413 1.67892 0.680619 1.80028L3.6314 4.75106C3.99919 5.10203 4.48805 5.29785 4.99644 5.29785C5.50483 5.29785 5.99368 5.10203 6.36148 4.75106L9.31161 1.80028C9.40157 1.71029 9.46283 1.59566 9.48764 1.47086C9.51246 1.34607 9.49972 1.21671 9.45103 1.09916C9.40234 0.981605 9.31989 0.881124 9.21411 0.810421C9.10832 0.739718 8.98394 0.701967 8.85671 0.70194Z"
+                          fill="currentColor"
+                        />
+                      </svg>
+                    </span>
+                  }
+                  style={{ width: 70 }}
+                >
+                  <Select.Option value="ru">RU</Select.Option>
+                  <Select.Option value="en">EN</Select.Option>
+                </Select>
+              </div>
+
+              <Select
+                defaultValue="Москва "
+                suffixIcon={
+                  <span className="bg-pink-500 text-[8px] text-white w-5 h-5 flex justify-center items-center rounded-full">
+                    <svg
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 10 6"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M8.85671 0.70194C8.68607 0.701976 8.52244 0.76979 8.4018 0.890465L5.45102 3.84124C5.32868 3.95866 5.16568 4.02423 4.99612 4.02423C4.82655 4.02423 4.66355 3.95866 4.54121 3.84124L1.59043 0.890465C1.46908 0.773259 1.30655 0.708405 1.13784 0.70987C0.969135 0.711337 0.807754 0.779006 0.688457 0.898303C0.56916 1.0176 0.50149 1.17898 0.500024 1.34769C0.498558 1.51639 0.563413 1.67892 0.680619 1.80028L3.6314 4.75106C3.99919 5.10203 4.48805 5.29785 4.99644 5.29785C5.50483 5.29785 5.99368 5.10203 6.36148 4.75106L9.31161 1.80028C9.40157 1.71029 9.46283 1.59566 9.48764 1.47086C9.51246 1.34607 9.49972 1.21671 9.45103 1.09916C9.40234 0.981605 9.31989 0.881124 9.21411 0.810421C9.10832 0.739718 8.98394 0.701967 8.85671 0.70194Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </span>
+                }
+                style={{ width: 120 }}
+                
+              >
+                <Select.Option value="moskva">Москва</Select.Option>
+                <Select.Option value="ams">Амстердам</Select.Option>
+                <Select.Option value="newest">Кито</Select.Option>
+              </Select>
+            </div>
             <Menu
               mode="inline"
               selectedKeys={[current]}
