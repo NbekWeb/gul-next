ff99993cc8951d19f885cd60d92f689d12d8c272
NbekWeb <nbekdev@gmail.com>
2024-12-26 12:44:54 +0500

    first commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,35 +11,30 @@ export default function RootLayout({ children }) {
           {/* Meta Tags */}
           <meta
             name="google-site-verification"
-            content="9TdsWg6r-S1EWJChiMZ7YB5mmsBPcVnm2KPhXhy6qvg"
+            content="unsFweJY8bLs_xFHYdP01fGEDlWWkTlUJqJ1LNET9NM"
           />
-          <meta name="yandex-verification" content="68a87b605d860b9e" />
+          <meta name="yandex-verification" content="ba4d062d2d49eb07" />
 
           {/* Yandex.Metrika Counter Script */}
           <script
             type="text/javascript"
-            src="https://mc.yandex.ru/metrika/tag.js"
-            async
-          ></script>
-
-          <script
             dangerouslySetInnerHTML={{
               __html: `
-              (function(m,e,t,r,i,k,a){
-                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();
-                for (var j = 0; j < document.scripts.length; j++) {
-                  if (document.scripts[j].src === r) { return; }
-                }
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-              })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {
+                    if (document.scripts[j].src === r) { return; }
+                  }
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+                })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-              ym(99314039, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true
-              });
-            `,
+                ym(99322723, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true
+                });
+              `,
             }}
           />
         </head>
@@ -47,7 +42,7 @@ export default function RootLayout({ children }) {
           <noscript>
             <div>
               <img
-                src="https://mc.yandex.ru/watch/99314039"
+                src="https://mc.yandex.ru/watch/99322723"
                 style={{ position: "absolute", left: "-9999px" }}
                 alt="Yandex Metrika"
               />
