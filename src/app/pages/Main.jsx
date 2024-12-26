8ab900e035dceb2fb920d44f31ca596eb4d4d732
NbekWeb <nbekdev@gmail.com>
2024-12-26 15:33:03 +0500

    first commit

--- src/app/pages/Main.jsx
+++ src/app/pages/Main.jsx
@@ -23,6 +23,18 @@ export default function HomePage() {
 
   const [loading, setLoading] = useState(0);
 
+  const [sitemap, setSitemap] = useState("");
+
+  useEffect(() => {
+    fetch("/sitemap")
+      .then((response) => response.text())
+      .then((data) => {
+        console.log(data);
+        setSitemap(data);
+      })
+      .catch((error) => console.error("Error fetching sitemap:", error));
+  }, []);
+
   const getBanner = async () => {
     setLoading((prev) => prev + 1);
     try {
